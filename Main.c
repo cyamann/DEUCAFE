a1090434c9fb18a487c53076e78f81440bf964ac
cyamann <100415384+cyamann@users.noreply.github.com>
2022-11-27 22:23:37 +0300

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define CUSTOMER_NUMBER 25
+#define CUSTOMER_ARRIVAL_TIME_MIN 1
+#define CUSTOMER_ARRIVAL_TIME_MAX 3
+#define REGISTER_NUMBER 5
+#define COFFEE_TIME_MIN 2
+#define COFFEE_TIME_MAX 5
+
+
+int main(){
+    printf("hello world");
+    return 0;
+}
